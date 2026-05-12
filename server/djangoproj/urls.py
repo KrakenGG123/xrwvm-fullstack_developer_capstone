6dadd28d83d69a400fcb82ac989ce0b9f3fd9117
KrakenGG123 <naisaldoshi@gmail.com>
2026-05-12 08:53:03 -0400

    Module 2 done

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', TemplateView.as_view(template_name="index.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('djangoapp/', include('djangoapp.urls')),
